cdda24a58e9e8d4922bb5c5f62d6ff54db36d8a5
Academy_learner <academy_learner@Academy-learners-MacBook-Air.local>
2021-12-03 08:38:07 +0200

    i program

---
+++ vee.js
@@ -0,0 +1,25 @@
+//console.log("vee is happy");
+
+//let i=6;
+//let name="Veronica"
+//console.log(i);
+//console.log("my name is "+name);
+var accType;
+const accountbalance=[200,100,300];
+for(let x=0;x<3;x++){
+    
+    if (
+  accountbalance[x]=200)
+  {
+let accType="savings";
+
+  }}
+  console.log(accType)
+/*  else if(accountbalance[x]=100)
+  {
+let accType="akanamali";
+
+  }
+  else{accType="premium";}
+    }
+    console.log(accType);*/
